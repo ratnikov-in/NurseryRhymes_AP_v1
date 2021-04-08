c77eaea2b6ade64e7a55f4948f7a53a503f709bb
Ратников Иван Николаевич <inratnikov@sberbank.ru>
2021-04-08 21:19:48 +0300

    edit text mode

--- resources/text.js
+++ resources/text.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   properties: {
     ...timestamps,
-    text : {type: 'richtext'},
+    text : {type: 'textarea'},
     categories: {isArray: true},
     _id: { isVisible: false },
     createdAt: { isVisible: false }
